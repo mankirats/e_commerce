025ff368d1965526f38aafbaaccd45da49a057c7
Mankirat Singh <mankirat95@gmail.com>
2022-09-30 21:39:03 +0530

    Promote component updated

--- src/Components/Promote.js
+++ src/Components/Promote.js
@@ -1,22 +1,25 @@
 import React from "react";
 import { Wrapper } from "./CustomStyle";
 import Product from "./Product";
+import Data from "./raw.json";
+
 function Promote(props) {
     return (
         <Wrapper>
-            <Product></Product>
-            <Product></Product>
-            <Product></Product>
-            <Product></Product>
-            <Product></Product>
-            <Product></Product>
-            <Product></Product>
-            <Product></Product>
-            <Product></Product>
-            <Product></Product>
-            <Product></Product>
-            <Product></Product>
-            <Product></Product>
+            {Data.slice(0, 8).map((pd, idx) => {
+                return (
+                    <Product
+                        key={idx}
+                        product={
+                            pd.title.length > 20
+                                ? pd.title.slice(0, 20) + "..."
+                                : pd.title
+                        }
+                        price={"\u20A8" + " " + pd.price}
+                        image={pd["images"]["medium"]["url"]}
+                    ></Product>
+                );
+            })}
         </Wrapper>
     );
 }
