b4e105aadd7292500bafc4b1e9cecc8fc39384a1
Mankirat Singh <mankirat95@gmail.com>
2022-09-30 21:37:15 +0530

    Button addded

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -1,7 +1,22 @@
 import React from "react";
 import { ButtonContainer } from "./CustomStyle";
+import { useState } from "react";
 function Button(props) {
-    return <ButtonContainer>{props.title}</ButtonContainer>;
+    const [cartValue, updateCartValue] = useState(0);
+    // const addToCart = (val) => {
+    //     console.log(val)
+    //     return val + 1;
+    // };
+    return (
+        <ButtonContainer
+            onClick={() => {
+                console.log(cartValue);
+                updateCartValue(cartValue + 1);
+            }}
+        >
+            {props.title}
+        </ButtonContainer>
+    );
 }
 
 export default Button;
